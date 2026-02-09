4c2038b7e6ecdb966bd6fefe94ad54ff0f19db47
Unicore Group <146719891+devunicoregroup@users.noreply.github.com>
2026-02-09 14:27:08 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -60,17 +60,22 @@ export default defineNuxtConfig({
         style: 'scroll-behavior: smooth;',
       },
       script: [
-      {
-        innerHTML: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TPHNPB5S');
-        `,
-          type: "text/javascript",
-          defer: true,
-      },
+        {
+            innerHTML: `
+              window.addEventListener('load', function() {
+                // Загружаем GTM через 3 секунды после полной загрузки страницы
+                // или можно добавить слушатель на 'mousemove' / 'touchstart'
+                setTimeout(function() {
+                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                  })(window,document,'script','dataLayer','GTM-TPHNPB5S');
+                }, 3000); 
+              });
+            `,
+            type: "text/javascript",
+          },
       ],
     },
 
