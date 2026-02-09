16dc32191f417718d2d583bb6875776c5fb69037
Unicore Group <146719891+devunicoregroup@users.noreply.github.com>
2026-02-09 14:21:10 +0200

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,19 +62,14 @@ export default defineNuxtConfig({
       script: [
       {
         innerHTML: `
-          window.addEventListener('load', function() {
-            // Загружаем GTM через 3 секунды после полной загрузки страницы
-            // или можно добавить слушатель на 'mousemove' / 'touchstart'
-            setTimeout(function() {
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TPHNPB5S');
-            }, 5000); 
-          });
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TPHNPB5S');
         `,
-        type: "text/javascript",
+          type: "text/javascript",
+          defer: true,
       },
       ],
     },
